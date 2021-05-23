bf950c4840c90ccd25c70b7e7955eb78fa6e387e
djackson29 <danika.c.jackson@gmail.com>
2021-05-23 14:10:30 -0700

    added variables and operators

---
+++ lesson02/calculator.js
@@ -0,0 +1,22 @@
+let w = 0;
+let x = 'true';
+let y = 2;
+let z = 3;
+
+if (24 / 8 === z) {
+  console.log(x);
+}
+
+if (w * y < 3) {
+  console.log('math is crazy');
+}
+
+for (let i = 0; i < 2; i++) {
+  console.log(x);
+}
+
+if (z + y - w === 6) {
+  console.log('z + y - w = 6');
+} else {
+  console.log('false');
+}
