9836e5819c180bbe5b0b5fc5778247669c892a91
djackson29 <danika.c.jackson@gmail.com>
2021-06-06 15:50:41 -0700

    completed challenge 2

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,6 @@
+let favoriteSpot = ['My', 'favorite', 'vacation', 'spot', 'is', 'The', 'Philippines.'];
+
+const favoriteSpotSentence = favoriteSpot.join(' ');
+
+console.log(favoriteSpotSentence);
+console.log(favoriteSpot[2]);
