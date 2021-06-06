386d9f0af59fd3a1b52a1ecf07327c4b019c79b9
djackson29 <danika.c.jackson@gmail.com>
2021-06-06 16:31:15 -0700

    complted challenge 3

---
+++ lesson03/exercise-three.js
@@ -0,0 +1,5 @@
+let vacationSpots = ['Hawaii', 'The Philippines', 'Italy', 'Virgin Islands', 'Greece'];
+
+vacationSpots.forEach(function(location) {
+  console.log(vacationSpots.indexOf(location) + ',' + location);
+})
