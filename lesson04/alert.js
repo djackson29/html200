cd6e267b2765abb929521a84a5b83ff3eb7a5085
djackson29 <danika.c.jackson@gmail.com>
2021-06-15 18:15:13 -0700

    completed lesson 4 assignment 1

---
+++ lesson04/alert.js
@@ -0,0 +1,18 @@
+function squareNumber() {
+  let input = prompt('Enter any number and I will square it for you!');
+  let result = 0;
+
+  result = Number(input) * Number(input);
+
+  alert(result);
+}
+
+function multiplier() {
+  let input = prompt('Enter first number');
+  let inputTwo = prompt('Enter second number');
+  let result = 0;
+
+  result = Number(input) * Number(inputTwo);
+
+  alert(result);
+}
